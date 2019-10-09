f90452c4ddec32d9e9458ba5a081724b8d0557ae
Yerba <yerbasoft@outlook.com>
2019-10-09 13:22:47 -0300

    Commit desd VS. prueba.

---
+++ CS/NET.CORE/SIR.Common/Log/Logger.cs
@@ -0,0 +1,175 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SIR.Common.Log
+{
+    /// <summary>
+    /// Clase administradora de logueo y traza de aplicaciones
+    /// </summary>
+    public class Logger
+    {
+        public LoggerConfig Config { get; private set; }
+        private ILogger _Logger { get; set; }
+
+        public Logger() : this("default", null) { }
+        public Logger(string name) : this(name, null) { }
+        public Logger(string name, LoggerConfig config)
+        {
+            this.Config = config ?? new LoggerConfig();
+            var nConfig = new NLog.Config.LoggingConfiguration();
+
+            var t = new NLog.Targets.FileTarget(name)
+            {
+                FileName = this.Config.LogFile.GetFullTemplateFileName(name),
+                Layout = this.Config.LogFile.LogTemplate
+            };
+            nConfig.AddTarget(t);
+            nConfig.LoggingRules.Add(new NLog.Config.LoggingRule(name, LogLevel.Trace, LogLevel.Error, t));
+
+            this._Logger = new NLog.LogFactory(nConfig).GetLogger(name);
+        }
+
+        #region Public Methods
+
+        public Logger Trace(string msg, object data = null) => Trace(new string[] { msg }, data);
+        public Logger Trace(IEnumerable<string> msgs, object data = null)
+        {
+            return DoLog(LogLevel.Trace, (this.Config.Console.ShowTraceLabel ? "TRACE " : ""), msgs, data, this.Config.Console.ShowTrace);
+        }
+
+        public Logger Debug(string msg, object data = null) => Debug(new string[] { msg }, data);
+        public Logger Debug(IEnumerable<string> msgs, object data = null)
+        {
+            return DoLog(NLog.LogLevel.Debug, (this.Config.Console.ShowDebugLabel ? "DEBUG " : ""), msgs, data, this.Config.Console.ShowDebug);
+        }
+
+        public Logger Info(string msg, object data = null) => Info(new string[] { msg }, data);
+        public Logger Info(IEnumerable<string> msgs, object data = null)
+        {
+            return DoLog(NLog.LogLevel.Info, (this.Config.Console.ShowInfoLabel ? "INFO " : ""), msgs, data, this.Config.Console.ShowInfo);
+        }
+
+        public Logger Warn(string msg, object data = null) => Warn(new string[] { msg }, data);
+        public Logger Warn(IEnumerable<string> msgs, object data = null)
+        {
+            return DoLog(NLog.LogLevel.Warn, (this.Config.Console.ShowWarnLabel ? "WARN " : ""), msgs, data, this.Config.Console.ShowWarn);
+        }
+
+        public Logger Error(string msg, object data = null) => Error(new string[] { msg }, data);
+        public Logger Error(Exception ex, object data = null) => Error(new string[] { ex.GetFullDescription() }, data);
+        public Logger Error(string msg, Exception ex, object data = null) => Error(new string[] { msg, ex.GetFullDescription() }, data);
+        public Logger Error(IEnumerable<string> msgs, object data = null)
+        {
+            return DoLog(NLog.LogLevel.Error, (this.Config.Console.ShowErrorLabel ? "ERROR " : ""), msgs, data, this.Config.Console.ShowError);
+        }
+
+        public DTO.Response Log(DTO.Response response)
+        {
+            if (response == null)
+                return response;
+
+            var info = response.Messages.Where(p => !p.EsError).Select(p => p.Text ?? p.Ex?.GetFullDescription());
+            var error = response.Messages.Where(p => p.EsError).Select(p => p.Text ?? p.Ex?.GetFullDescription());
+
+            Info(info);
+            Error(error);
+
+            return response;
+        }
+
+        public DTO.Response<T> Log<T>(DTO.Response<T> response)
+        {
+            if (response == null)
+                return response;
+
+            var info = response.Messages.Where(p => !p.EsError).Select(p => p.Text ?? p.Ex?.GetFullDescription());
+            var error = response.Messages.Where(p => p.EsError).Select(p => p.Text ?? p.Ex?.GetFullDescription());
+
+            Info(info);
+            Error(error);
+
+            return response;
+        }
+
+        #endregion
+
+        #region Core Methods
+
+        /// <summary>
+        /// Funcion genérica para todos los tipos de logs
+        /// </summary>
+        /// <param name="level">Nivel de log</param>
+        /// <param name="prefix">prefijo a mostrar para el nivel</param>
+        /// <param name="msgs">lista de mensajes a mostrar</param>
+        /// <param name="data">objeto adicional a mostrar sus datos</param>
+        /// <param name="showInConsole">indica si se debe mostrar por consola según la configuracion</param>
+        /// <returns></returns>
+        private Logger DoLog(LogLevel level, string prefix, IEnumerable<string> msgs, object data, bool showInConsole)
+        {
+            if (this.Config.LogFile.ShowInLogFile)
+            {
+                var toFile = msgs.ToList();
+                if (data != null)
+                    toFile.Add("Reference: " + Newtonsoft.Json.JsonConvert.SerializeObject(data));
+
+                DoShowInLogFile(level, toFile);
+            }
+
+            if (this.Config.Console.ShowInConsole && showInConsole)
+            {
+                DoShowInConsole(msgs.Select(p => prefix + p));
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Muestra una lista de mensajes ya procesados en la consola
+        /// </summary>
+        /// <param name="msgs"></param>
+        private void DoShowInConsole(IEnumerable<string> msgs)
+        {
+            try
+            {
+                var prefix = "";
+                if (!string.IsNullOrEmpty(this.Config.Console.DateFormat))
+                    prefix = DateTime.Now.ToString(this.Config.Console.DateFormat) + " ";
+
+                if (!string.IsNullOrEmpty(prefix))
+                {
+                    if (msgs.Count() > 1)
+                    {
+                        System.Console.WriteLine(prefix);
+                        prefix = "\t";
+                    }
+                }
+
+                foreach (var msg in msgs)
+                    System.Console.WriteLine($"{prefix}{msg}");
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Imprime una lista de resultados a una archivo d elog
+        /// </summary>
+        /// <param name="level">nivel de log</param>
+        /// <param name="msgs">lista de mensajes</param>
+        private void DoShowInLogFile(LogLevel level, IEnumerable<string> msgs)
+        {
+            try
+            {
+                lock (this._Logger)
+                {
+                    foreach (var msg in msgs)
+                        this._Logger.Log(level, msg);
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+    }
+}
---
+++ CS/NET.CORE/SIR.Common/Log/LoggerConfig.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIR.Common.Log
+{
+    /// <summary>
+    /// Estructura de configuración del Logger
+    /// </summary>
+    public class LoggerConfig
+    {
+        public class ConsoleConfig
+        {
+            /// <summary>
+            /// Indica si se utiliza la salida por consola
+            /// </summary>
+            public bool ShowInConsole { get; set; } = true;
+            /// <summary>
+            /// Formato de fecha a mostrar en el log. Establecer null para no mostrar la fecha
+            /// </summary>
+            public string DateFormat { get; set; } = "dd-MM-yyyy HH:mm:ss.ff";
+
+            /// <summary>
+            /// Indica si se muestran por consola los logs de tipo TRACE
+            /// </summary>
+            public bool ShowTrace { get; set; } = true;
+            /// <summary>
+            /// Indica si se muestra por consola la leyenda "TRACE" para los logs de tipo TRACE
+            /// </summary>
+            public bool ShowTraceLabel { get; set; } = true;
+
+            /// <summary>
+            /// Indica si se muestran por consola los logs de tipo DEBUG
+            /// </summary>
+            public bool ShowDebug { get; set; } = true;
+            /// <summary>
+            /// Indica si se muestra por consola la leyenda "DEBUG" para los logs de tipo DEBUG
+            /// </summary>
+            public bool ShowDebugLabel { get; set; } = true;
+
+            /// <summary>
+            /// Indica si se muestran por consola los logs de tipo INFO
+            /// </summary>
+            public bool ShowInfo { get; set; } = true;
+            /// <summary>
+            /// Indica si se muestra por consola la leyenda "INFO" para los logs de tipo INFO
+            /// </summary>
+            public bool ShowInfoLabel { get; set; } = true;
+
+            /// <summary>
+            /// Indica si se muestran por consola los logs de tipo WARN
+            /// </summary>
+            public bool ShowWarn { get; set; } = true;
+            /// <summary>
+            /// Indica si se muestra por consola la leyenda "WARN" para los logs de tipo WARN
+            /// </summary>
+            public bool ShowWarnLabel { get; set; } = true;
+
+            /// <summary>
+            /// Indica si se muestran por consola los logs de tipo ERROR
+            /// </summary>
+            public bool ShowError { get; set; } = true;
+            /// <summary>
+            /// Indica si se muestra por consola la leyenda "ERROR" para los logs de tipo ERROR
+            /// </summary>
+            public bool ShowErrorLabel { get; set; } = true;
+        }
+
+        public class LogFileConfig
+        {
+            /// <summary>
+            /// Indica si se deben exponer los logs por archivo
+            /// </summary>
+            public bool ShowInLogFile { get; set; } = true;
+
+            /// <summary>
+            /// Devuelve o establece el template a utilizar al registrar un log
+            /// </summary>
+            public string LogTemplate { get; set; } = "${longdate} ${uppercase:${level}} ${message}";
+
+            /// <summary>
+            /// Indica la subcarpeta/s desde el directorio de la aplicación, donde se guardará el log.
+            /// </summary>
+            public string SubFolder { get; set; } = "log/";
+
+            /// <summary>
+            /// Indica el formato para el nombre de archivo
+            /// </summary>
+            public string FileName { get; set; } = "${shortdate}.log";
+
+            /// <summary>
+            /// Indica si el nombre de archivo de salida de log utilizará como prefijo el nombre establecido para el logger
+            /// </summary>
+            public bool UseLoggerNameAsFileNamePrefix { get; set; } = true;
+
+            /// <summary>
+            /// Devuelve la ruta completa en forma de template del archivo de log.
+            /// </summary>
+            /// <returns></returns>
+            public string GetFullTemplateFileName(string loggerName)
+            {
+                var filename = "${basedir}/";
+
+                if (!string.IsNullOrEmpty(this.SubFolder))
+                {
+                    if (this.SubFolder.StartsWith("/") || this.SubFolder.StartsWith("\\"))
+                        filename += this.SubFolder.Substring(1);
+                    else
+                        filename += this.SubFolder;
+                }
+
+                if (!filename.EndsWith("/") && !filename.EndsWith("\\"))
+                    filename += "/";
+
+                if (this.UseLoggerNameAsFileNamePrefix)
+                {
+                    filename += loggerName;
+                    if (string.IsNullOrEmpty(this.FileName))
+                        filename += ".log";
+                    else
+                        filename += " ";
+                }
+
+                if (!string.IsNullOrEmpty(this.FileName))
+                {
+                    if (this.FileName.StartsWith("/") || this.FileName.StartsWith("\\"))
+                        filename += this.FileName.Substring(1);
+                    else
+                        filename += this.FileName;
+                }
+
+                return filename;
+            }
+        }
+
+        /// <summary>
+        /// Configuración para el output por consola
+        /// </summary>
+        public ConsoleConfig Console { get; set; } = new ConsoleConfig();
+
+        /// <summary>
+        /// Configuración para el output por log de archivo
+        /// </summary>
+        public LogFileConfig LogFile { get; set; } = new LogFileConfig();
+    }
+}
